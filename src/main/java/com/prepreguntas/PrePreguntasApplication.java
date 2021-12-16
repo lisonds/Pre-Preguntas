1bd0d478c1ea426c2eac7d267c6f87e114ad8b9d
Tulio lison <35206029+lisonds@users.noreply.github.com>
2021-12-15 23:45:22 -0500

    comensemos
    lisonds

---
+++ src/main/java/com/prepreguntas/PrePreguntasApplication.java
@@ -0,0 +1,13 @@
+package com.prepreguntas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrePreguntasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrePreguntasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prepreguntas/PrePreguntasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepreguntas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrePreguntasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
