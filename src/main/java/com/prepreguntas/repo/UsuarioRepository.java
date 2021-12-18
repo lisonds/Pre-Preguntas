49bc81f203131c91b35141b8ae6fc0bdec27b16a
hitson-lm <hitson.lm@gmail.com>
2021-12-17 21:57:01 -0500

    agregando anotacion

--- src/main/java/com/prepreguntas/repo/UsuarioRepository.java
+++ src/main/java/com/prepreguntas/repo/UsuarioRepository.java
@@ -1,9 +1,10 @@
 package com.prepreguntas.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.prepreguntas.entity.Usuario;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
 }
