152bc624a2f0bfd6132f816d51ee6e4a8c142c27
Tulio lison <35206029+lisonds@users.noreply.github.com>
2021-12-20 16:05:45 -0500

    1

--- src/main/java/com/prepreguntas/controller/loginController.java
+++ src/main/java/com/prepreguntas/controller/loginController.java
@@ -4,28 +4,37 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.prepreguntas.entity.Usuario;
 import com.prepreguntas.repo.UsuarioRepository;
 
-@RestController
-@RequestMapping("/login")
+@Controller
+
 public class loginController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
-	@GetMapping
-	List<Usuario> Logint() {
+	@GetMapping("/login")
+	public String Logint() {
 		
-		return usuarioRepository.findAll();
+		return "view/login";
 	}
-	@PostMapping
-	Usuario crear(@RequestBody Usuario usuario) {
-		return usuarioRepository.save(usuario);
+	@PostMapping("/login")
+	public String Loguear(Model model,
+			@RequestParam String nombre,
+			@RequestParam String email,
+			@RequestParam String password,
+			@RequestParam String password2) {
+		model.addAttribute("nombre",nombre);
+		model.addAttribute("email",email);
+		model.addAttribute("password",password);
+		model.addAttribute("password2",password2);
+		return "prueba";
 	}
+	
 }
