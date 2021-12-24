aee4f1256e613b3004a7e32ce88536d0f08ab414
Tulio lison <35206029+lisonds@users.noreply.github.com>
2021-12-24 00:14:36 -0500

    img

--- src/main/java/com/prepreguntas/controller/loginController.java
+++ src/main/java/com/prepreguntas/controller/loginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.prepreguntas.DAO.IUsuarioDao;
+
 import com.prepreguntas.entity.Usuario;
 import com.prepreguntas.service.IUsuarioService;
 
