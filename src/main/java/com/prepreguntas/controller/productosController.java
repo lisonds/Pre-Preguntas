ed67251160f596b838bc432ed21db645de501aaa
hitson-lm <hitson.lm@gmail.com>
2021-12-29 23:40:29 -0500

    cambios en produc.html

--- src/main/java/com/prepreguntas/controller/productosController.java
+++ src/main/java/com/prepreguntas/controller/productosController.java
@@ -2,13 +2,17 @@ package com.prepreguntas.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/productos")
 public class productosController {
 	
-	@GetMapping("/productos")
+	@GetMapping("")
 	public String verProducto() {
 		return "productos/produc";
 	}
+
+
 
 }
