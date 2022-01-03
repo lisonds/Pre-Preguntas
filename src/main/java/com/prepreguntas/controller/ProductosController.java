7c2460c38785442f84aa997b1b57ad28048bde36
hitson-lm <hitson.lm@gmail.com>
2022-01-02 23:16:23 -0500

    Mostrando productos registrados

--- src/main/java/com/prepreguntas/controller/ProductosController.java
+++ src/main/java/com/prepreguntas/controller/ProductosController.java
@@ -1,8 +1,11 @@
 package com.prepreguntas.controller;
 
 import com.prepreguntas.entity.Producto;
+import com.prepreguntas.service.ProductoServiceImpl;
 import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,12 @@ public class ProductosController {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductosController.class);
 
+	@Autowired
+	private ProductoServiceImpl productoService;
+
 	@GetMapping("")
-	public String verProducto() {
+	public String verProducto(Model model) {
+		model.addAttribute("productos", productoService.findAll());
 		return "productos/produc";
 	}
 
@@ -26,6 +33,7 @@ public class ProductosController {
 	@PostMapping("/guardar")
 	public String guardar(Producto producto) {
 		LOGGER.info("Guardando producto: {}", producto);
+		productoService.saveOne(producto);
 		return "redirect:/productos";
 	}
 
