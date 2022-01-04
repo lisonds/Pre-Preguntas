14ef24f523d84ab1756ab3d7a9a2e925b6341a3b
hitson-lm <hitson.lm@gmail.com>
2022-01-03 22:28:22 -0500

    configuracion para mostrar images de producto

--- src/main/java/com/prepreguntas/controller/ProductosController.java
+++ src/main/java/com/prepreguntas/controller/ProductosController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/productos")
@@ -29,7 +30,8 @@ public class ProductosController {
 
 	@GetMapping("")
 	public String verProducto(Model model) {
-		model.addAttribute("productos", productoService.findAll());
+		List<Producto> productos = productoService.findAll();
+		model.addAttribute("productos",productos);
 		return "productos/produc";
 	}
 
