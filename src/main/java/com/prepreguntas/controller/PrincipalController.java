f9d7146950b7ae4017e2c434782c656d94232133
Tulio lison <35206029+lisonds@users.noreply.github.com>
2022-01-04 19:05:10 -0500

    idDeManeraGlobal

--- src/main/java/com/prepreguntas/controller/PrincipalController.java
+++ src/main/java/com/prepreguntas/controller/PrincipalController.java
@@ -12,6 +12,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelExtensionsKt;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,67 +26,70 @@ import com.prepreguntas.service.IUsuarioService;
 
 @Controller
 public class PrincipalController {
-		@Autowired
-		private IUsuarioService usuarioService;
-		
-		
-		@PostMapping("/principal")
-		public String BuscarCorreo(@Valid Usuario usuario, BindingResult result,Map<String, Object> model) {
-			
-			int id=0;
-			List<Usuario> lista = usuarioService.busrcarEmail(usuario.getCorreo());
-			
-			if(lista.size()==0) {
-				return"redirect:/login";
-			}else {
-				for(Usuario v:lista) {
-					
-					id=v.getId();
-				}
-			Usuario usuario1=usuarioService.findOne(id);
-				model.put("nombre",usuario1.getNombre());
-				model.put("correo",usuario1.getCorreo());
-				model.put("id",usuario1.getId());
-				Publicacion publicacion=new Publicacion();
-				publicacion.setUsuario(usuario1);
-				
-				model.put("publicacion",publicacion);
-				model.put("usuario", usuario1);
-				model.put("titulo"," crea tu publicacion");
-				
-				System.out.println("prueba "+publicacion.getIdPublicacion());
-				return"principal/index";
+	@Autowired
+	private IUsuarioService usuarioService;
+	private int id;
+
+	@PostMapping("/usuarioprincipal")
+	public String BuscarCorreo(@Valid Usuario usuario, BindingResult result, Map<String, Object> model) {
+
+		List<Usuario> lista = usuarioService.busrcarEmail(usuario.getCorreo());
+
+		if (lista.size() == 0) {
+			return "redirect:/login";
+		} else {
+			for (Usuario v : lista) {
+
+				id = v.getId();
 			}
 			
-			}
-		@PostMapping("/publicacion/form")
-		public String CapturarPublicacion(Publicacion publicacion,Model model, @RequestParam("file") MultipartFile imagen) {
-			/*EXTRAENDO IMAGEN PARA SUBIR AL BASE DE DATOS*/
-			System.out.println("Antes del if");
-			if(!imagen.isEmpty()) {
-				System.out.println("llego asta aqui");
-				Path directoriaoRecursos=Paths.get("src//main//resources//static/uploads");
-				String rootPath=directoriaoRecursos.toFile().getAbsolutePath();
-				try {
-					byte[] bytes=imagen.getBytes();
-					Path rutaCompleta=Paths.get(rootPath+"/"+imagen.getOriginalFilename());
-					Files.write(rutaCompleta, bytes);
-					System.out.println("has subido un imagen "+ imagen.getOriginalFilename());
-					publicacion.setImagen(imagen.getOriginalFilename());
-				} catch (IOException e) {
-					
-					e.printStackTrace();
-				}
-				
-			}
-			
-			System.out.println(publicacion.getContenido());
-			System.out.println(publicacion.getImagen());
 			return "redirect:/principal";
 		}
-		
-		
-		
+
+	}
+	
+	@GetMapping("/principal")
+	public String principal(Map<String, Object> model) {
+		Usuario usuario1 = usuarioService.findOne(id);
+		model.put("nombre", usuario1.getNombre());
+		model.put("correo", usuario1.getCorreo());
+		model.put("id", usuario1.getId());
+		Publicacion publicacion = new Publicacion();
+		publicacion.setUsuario(usuario1);
+
+		model.put("publicacion", publicacion);
+		model.put("usuario", usuario1);
+		model.put("titulo", " crea tu publicacion");
+
+		System.out.println("prueba " + publicacion.getIdPublicacion());
+		return "principal/index";
+	}
+
+	@PostMapping("/publicacion/form")
+	public String CapturarPublicacion(Publicacion publicacion, Model model,
+			@RequestParam("file") MultipartFile imagen) {
+		/* EXTRAENDO IMAGEN PARA SUBIR AL BASE DE DATOS */
+		System.out.println("Antes del if");
+		if (!imagen.isEmpty()) {
+			System.out.println("llego asta aqui");
+			Path directoriaoRecursos = Paths.get("src//main//resources//static/uploads");
+			String rootPath = directoriaoRecursos.toFile().getAbsolutePath();
+			try {
+				byte[] bytes = imagen.getBytes();
+				Path rutaCompleta = Paths.get(rootPath + "/" + imagen.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+				System.out.println("has subido un imagen " + imagen.getOriginalFilename());
+				publicacion.setImagen(imagen.getOriginalFilename());
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+
+		}
+
+		System.out.println(publicacion.getContenido());
+		System.out.println(publicacion.getImagen());
+		return "redirect:/principal";
+	}
+
 }
-
-
